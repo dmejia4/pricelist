7eecea6fb49a87bcb6931751a1a9e1fa74cf1547
Daniel Mejia <daniel_mejia@bhdleon.com.do>
2022-08-31 15:44:42 +0200

    Project Done

--- src/test/java/com/vass/pricelist/repository/PriceRepositoryTest.java
+++ src/test/java/com/vass/pricelist/repository/PriceRepositoryTest.java
@@ -31,8 +31,6 @@ class PriceRepositoryTest {
                                                   "(4, 1, '2020-06-15 16:00:00', '2020-12-31 23:59:59', 4, '35455', 1, 38.95, 'EUR')").executeUpdate();
     }
 
-
-
     @Test
     void findByBrandIdProductIdAndApplicationDateTest1() {
         Price price = priceRepository.findByBrandIdProductIdAndApplicationDate(Timestamp.valueOf("2020-06-14 10:00:00"), "35455", 1).get(0);
